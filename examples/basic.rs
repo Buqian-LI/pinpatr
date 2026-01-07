e8ec1b87a77a91bc07fe93eb8d8b9e8fb3ac1054
Buqian-LI <buhchian.li@gmail.com>
2026-01-07 01:33:13 +0100

    updated example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,11 @@
-use pinpatr::{Pinpatr, Token};
+use siphon::{Siphon, Token};
 
 fn main() {
-    let converter: Pinpatr = Pinpatr::new().text(vec!["ni3".to_string(), "hao3".to_string()]);
     println!("[Input] ni3 hao3");
 
+    let converter: Siphon = Siphon::new("ni3 hao3");
     let tokens: Vec<Token> = converter.tokenize().unwrap();
     let result: String = converter.transform(tokens).unwrap();
+
     println!("[Output] {}", result);
 }
